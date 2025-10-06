18e990890f5c649d104a2a7d5df78eab55aebc1d
zulin <zulin.gu@qq.com>
2025-10-06 08:07:35 +0800

    设置时间

--- Core/Src/rtc.c
+++ Core/Src/rtc.c
@@ -63,8 +63,8 @@ void MX_RTC_Init(void)
 
   /** Initialize RTC and set the Time and Date
   */
-  sTime.Hours = 0x0;
-  sTime.Minutes = 0x0;
+  sTime.Hours = 0x8;
+  sTime.Minutes = 0x30;
   sTime.Seconds = 0x0;
   sTime.SubSeconds = 0x0;
   sTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
