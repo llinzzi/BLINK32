850786428f67a9f219097733901837e5947df1d9
zulin <zulin.gu@qq.com>
2025-10-06 08:11:17 +0800

    设置时间

--- Core/Src/rtc.c
+++ Core/Src/rtc.c
@@ -76,7 +76,7 @@ void MX_RTC_Init(void)
   sDate.WeekDay = RTC_WEEKDAY_MONDAY;
   sDate.Month = RTC_MONTH_JANUARY;
   sDate.Date = 0x1;
-  sDate.Year = 0x0;
+  sDate.Year = 0x25;
 
   if (HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BCD) != HAL_OK)
   {
